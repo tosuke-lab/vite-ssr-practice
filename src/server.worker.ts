67e58c23a0160e9172c42630978bc88080a24d6e
tosuke <tasukeprg@gmail.com>
2022-01-28 00:46:28 +0900

    refactor: rewrite worker server

--- src/server.worker.ts
+++ src/server.worker.ts
@@ -1,17 +1,56 @@
-// @ts-nocheck
 import indexHtml from "../dist/client/index.html?raw";
+import { renderToStream } from "./entry-server";
 import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 
-const textEncoder = new TextEncoder();
-(globalThis as any).Buffer = {
-  from(chunk: string) {
-    return textEncoder.encode(chunk);
-  },
-};
+async function handleRequest(event: FetchEvent) {
+  const url = new URL(event.request.url);
 
-(globalThis as any).setImmediate = (cb: () => void) => {
-  setTimeout(cb, 0);
-};
+  if (/\.(js|css|ico)$/.test(url.pathname)) {
+    return await handleAsset(event, url);
+  }
+
+  const { signal, cancel } = timeoutSignal(10 * 1000);
+
+  try {
+    const { readable, writable } = new TransformStream();
+    const writer = writable.getWriter();
+    signal.addEventListener("abort", () => {
+      writer.close();
+    });
+
+    const { statusCode, stream } = await renderToStream({
+      signal,
+      headElements: indexHtml,
+    });
+
+    const reader = stream.getReader();
+    void (async () => {
+      try {
+        for (;;) {
+          const r = await reader.read();
+          if (r.done) break;
+          await writer.write(r.value);
+        }
+      } finally {
+        writer.close();
+      }
+    })();
+
+    return new Response(readable, {
+      status: statusCode,
+      headers: {
+        "content-type": "text/html",
+      },
+    });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : String(err);
+    return new Response(message, {
+      status: 500,
+    });
+  } finally {
+    cancel();
+  }
+}
 
 async function handleAsset(event: FetchEvent, url: URL) {
   try {
@@ -21,68 +60,26 @@ async function handleAsset(event: FetchEvent, url: URL) {
     const pathname = url.pathname;
     return new Response(`"${pathname}" not found`, {
       status: 404,
-      statusText: "not found",
     });
   }
 }
 
+function timeoutSignal(timeoutMs: number) {
+  const controller = new AbortController();
+  const signal = controller.signal;
+  const timeout = setTimeout(() => {
+    controller.abort();
+  }, timeoutMs);
+  const cancel = () => {
+    clearTimeout(timeout);
+  };
+  return {
+    signal,
+    cancel,
+  } as const;
+}
+
 addEventListener("fetch", (eventBase) => {
   const event = eventBase as FetchEvent;
-  const url = new URL(event.request.url);
-
-  if (/\.(js|css)$/.test(url.pathname)) {
-    event.respondWith(handleAsset(event, url));
-    return;
-  }
-
-  event.respondWith(
-    import("./entry-server")
-      .then(async (mod) => {
-        const controller = new AbortController();
-
-        const signal = controller.signal;
-
-        const { readable, writable } = new TransformStream();
-        const writer = writable.getWriter();
-
-        const { statusCode, stream } = await mod.renderToStream({
-          signal,
-          headElements: indexHtml,
-        });
-
-        // タイムアウトを付与することで，Workerの稼動時間を延長する
-        const timeout = setTimeout(() => {
-          controller.abort();
-        }, 10 * 1000);
-
-        signal.addEventListener("abort", () => {
-          writer.close();
-        });
-
-        const reader = stream.getReader();
-        void (async () => {
-          try {
-            for (;;) {
-              const r = await reader.read();
-              if (r.done) break;
-              await writer.write(r.value);
-            }
-          } finally {
-            clearTimeout(timeout);
-            writer.close();
-          }
-        })();
-        return new Response(readable, {
-          status: statusCode,
-          headers: {
-            "content-type": "text/html",
-          },
-        });
-      })
-      .catch((error) => {
-        return new Response(error.message, {
-          status: 500,
-        });
-      })
-  );
+  event.respondWith(handleRequest(event));
 });
